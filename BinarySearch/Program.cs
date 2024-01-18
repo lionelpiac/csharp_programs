6e4ee70ff3b31bc15a3889e1863ee7d8ae8f57b2
Lionel Piacquadio <lionel.piacquadio@activeops.com>
2024-01-18 21:37:54 +0000

    add binary search explanation

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -66,3 +66,6 @@ class Program
         return -1;
     }
 }
+
+
+// In computer science, binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array. Binary search compares the target value to the middle element of the array.
